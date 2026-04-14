c8fac4f4f5b6c791f400d6bfaa681e10c22af52a
Lirika Marteti <lm73112@ubt-uni.net>
2026-04-14 22:08:19 +0200

    Add files via upload

---
+++ 2026_04_14_194703_create_bookmarks_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->integer('faqja');
+            $table->text('shenime')->nullable();
+            $table->timestamp('data_krijimit')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+};
